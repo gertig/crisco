a885f79216261f3a0c2c4e00e4e32f4176fba542
AndrewGertig <andrew.gertig@gmail.com>
2011-09-26 09:13:27 -0400

    Add click tracking

--- app.rb
+++ app.rb
@@ -1,28 +1,21 @@
 require 'sinatra'
 require 'redis'
+require 'json'
 
 set :env, (ENV['RACK_ENV'] ? ENV['RACK_ENV'].to_sym : :development)
 #set :env, (ENV['RACK_ENV'] ? :production : :development)
 
 configure :development do
   ENV["REDISTOGO_URL"] = 'redis://localhost:6379' 
-  
   #redis = Redis.new
   puts "Interesting, the environment is development"
 end
 
 configure do
   uri = URI.parse(ENV["REDISTOGO_URL"])
-  puts uri.host
-  puts uri.port
-  puts uri.password
+  puts "Host = #{uri.host}, Port = #{uri.port}, Password = #{uri.password}"
   REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password) #REDIS is in all caps so its treated as a fixed variable
-  #REDIS = Redis.new
-  puts "I hope this parsed"
 end
-
-
-#redis = Redis.new
 
 
 ####
@@ -42,9 +35,7 @@ end
 # ROUTES
 ####
 get '/' do
-  # if ENV['RACK_ENV']
-  #   @env = ENV['RACK_ENV'].to_sym
-  # end
+  # content_type :json if this was supposed to render json
   erb :index
 end
 
@@ -52,15 +43,25 @@ post '/' do
   if params[:url] and not params[:url].empty?
     @shortcode = random_string(5)
     REDIS.setnx("links:#{@shortcode}", params[:url]) # .setnx = Set if n exists
+    REDIS.setnx("links:#{@shortcode}:clicks", 0)
     
     #The ‘links:’ part of the key isn’t strictly required, but it’s good practice 
     #to split your Redis keys into namespaces so if you decide later on to store 
     #more information in the same database,
   end
-  erb :index
+  #erb :index
+  { :url => "http://ten.io/#{@shortcode}" }.to_json
+end
+
+post '/stats' do
+    @end_url = REDIS.get("links:#{params[:shortcode]}")
+    @clicks = REDIS.get("links:#{params[:shortcode]}:clicks")
+    { :end_url => @end_url, :clicks => @clicks }.to_json
 end
 
 get '/:shortcode' do
   @url = REDIS.get("links:#{params[:shortcode]}")
+  REDIS.incr("links:#{params[:shortcode]}:clicks")
+  
   redirect @url || '/'
 end
